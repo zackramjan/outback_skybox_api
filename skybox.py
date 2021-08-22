7dba08949cd43b772b163d1bf6484519fa52a766
zackramjan <zack.hr@gmail.com>
2021-08-22 04:48:45 +0000

    better comments

--- skybox.py
+++ skybox.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-import SkyboxAPI
 import json
 import sys
+import SkyboxAPI
 
 #this is an example usage of the SkyboxAPI to retrieve and print various metrics
 def main(argv=None): 
     # make sure you have enabled remote login in Global Settings -> System -> Remote Security -> Enable Remote Login. 
-    s = SkyboxAPI()
+    s = SkyboxAPI.SkyboxAPI()
 
     # The web interface will be running on port 3000. you can check your skybox IP address on the skybox in the Global Settings -> Network
     # It will attempt to use the outback default remote access account of username = "I" and password = "skybox"
@@ -18,14 +18,21 @@ def main(argv=None):
     # loginStatus = s.login("http://192.168.1.142:3000") 
     # print(loginStatus)
     
-    #Lets get the current status and print a few metrics
+    # Lets get the current status and print a few metrics
     status = s.getStatus()
+
+    # display the status of the pv input
+    # the status is a value from 0 to 6
     print(status['pv_status'])
 
-    #for every 'metric', there is usually a corresponding 'metric_property' which describes the values the metric returns
+    # for every 'metric', there is usually a corresponding 'metric_property' which describes the range of values the metric returns.
+    # here we can see that 1 means "producing" and 3 means "sleeping"
     print(status['pv_status_property'])
 
+    #print the load from the load panel
     print(status['load_combined_wattage_sum'])
+
+    #printing the corresponding property, describes the units etc
     print(status['load_combined_wattage_sum_property'])
 
 
