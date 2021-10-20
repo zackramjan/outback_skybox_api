37e334faf96c980360a958b81b02048d8348bd7b
zackramjan <zack.hr@gmail.com>
2021-10-20 23:32:57 +0000

    fixed pull in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 cd /outback_skybox_api
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
-git pull -b skybox https://github.com/zackramjan/outback_skybox_api.git
+git pull 
 /usr/bin/env python3 /outback_skybox_api/skyboxInflux.py
