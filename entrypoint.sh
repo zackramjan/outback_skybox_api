ed760bbad1fd0440d4d35e9131e750f26aefda62
zackramjan <zack.hr@gmail.com>
2021-10-20 14:07:30 +0000

    added info to prevent git warnings

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd /outback_skybox_api
-git pull https://github.com/zackramjan/outback_skybox_api.git
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+git pull -b skybox https://github.com/zackramjan/outback_skybox_api.git
 /usr/bin/env python3 /outback_skybox_api/skyboxInflux.py
